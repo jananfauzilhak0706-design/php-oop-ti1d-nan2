e17111335c9a8f12ac8633dadcbfbbedf55b6ee9
jananfauzilhak0706-design <jananfauzilhak0706@gmail.com>
2026-05-26 15:24:44 +0700

    tugas

---
+++ Buah.php
@@ -0,0 +1,33 @@
+<?php
+class Fruit {
+  public $name;
+  public $color;
+
+  function set_details($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+
+  function get_details() {
+    echo "Name: " . $this->name . ". Color: " . $this->color .".<br>";
+  }
+}
+
+// Create an object named $apple from the Fruit class
+$apple = new Fruit();
+$apple->set_details('Apple', 'Red'); // Set property values
+$apple->get_details(); // Get output
+
+// Create an object named $banana from the Fruit class
+$banana = new Fruit();
+$banana->set_details('Banana', 'Yellow'); // Set property values
+$banana->get_details(); // Get output
+?>
+PHP - instanceof
+You can use the instanceof keyword to check if an object belongs to a specific class:
+
+Example
+<?php
+$apple = new Fruit();
+var_dump($apple instanceof Fruit);
+?>
